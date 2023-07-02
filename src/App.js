292267136d28d8ba4c027169b81b687753ac00f1
kristina130204 <kristinadanylova1304@gmail.com>
2023-07-02 23:01:59 +0300

    Started developing register page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './style.scss'
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='Register'>
+      <div className="formWrapper">
+        <div className="blur"></div>
+        <div className="blur"></div>
+        <span className="logo">PrivateChat</span>
+        <span className="title">Register</span>
+        <form>
+            <input type="text" name="" id="" placeholder='Name' />
+            <input type="email" name="" id="" placeholder='Email' />
+            <input type="password" name="" id="" placeholder='Password' />
+            <input type="file" name="" id="" />
+            <button className="button">Sign Up</button>
+        </form>
+        <p>Already have an account? Login</p>
+      </div>
+    </div>
+  )
+}
+
+export default Register
